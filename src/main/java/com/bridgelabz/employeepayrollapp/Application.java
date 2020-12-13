988f5f557d793d88611e8e8a46526c32f565dc2e
madhu179 <madhubabu232343@gmail.com>
2020-12-13 18:24:55 +0530

    Adding code to use lombok library for logging

--- src/main/java/com/bridgelabz/employeepayrollapp/Application.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/Application.java
@@ -2,11 +2,22 @@ package com.bridgelabz.employeepayrollapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+//        SpringApplication.run(Application.class, args);
+    	ApplicationContext context = SpringApplication
+    									.run(Application.class, args);
+    
+    	log.info("Employee Payroll App Started in {} Environment",
+    			context.getEnvironment().getProperty("environment"));
+    	log.info("Employee Payroll DB User is {} Environment",
+    			context.getEnvironment().getProperty("spring.datasource.username"));
     }
 }
 
