5891d1fe940458e85eeda7a901f433c5675e0c95
madhu179 <madhubabu232343@gmail.com>
2020-12-14 15:34:58 +0530

    Adding code to add validation to name and salary field and validate them in rest call

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -1,8 +1,14 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
+
 public class EmployeePayrollDTO {
 	
+	@Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message="Employee name Invalid")
 	public String name;
+	
+	@Min(value = 500, message="Min Wage should be more than 500")
 	public long salary;
 	
 	@Override
