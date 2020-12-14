ed9073dd8801e5779e69d35cc24d93a86dd27b4f
madhu179 <madhubabu232343@gmail.com>
2020-12-14 16:34:21 +0530

    Adding code to throw errors if employee id is not found in Rest Request

--- src/main/java/com/bridgelabz/employeepayrollapp/exceptions/EmployeePayrollExceptionHandler.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/exceptions/EmployeePayrollExceptionHandler.java
@@ -17,7 +17,7 @@ public class EmployeePayrollExceptionHandler {
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(
-			MethodArgumentNotValidException exception) {
+										MethodArgumentNotValidException exception) {
 		List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
 		List<String> errMesg = errorList.stream()
 										.map(objErr -> objErr.getDefaultMessage())
@@ -25,5 +25,13 @@ public class EmployeePayrollExceptionHandler {
 		ResponseDTO responseDTO = new ResponseDTO("Exception while processing REST Request", errMesg);
 		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
 				}
+	
+	@ExceptionHandler(EmployeePayrollException.class)
+	public ResponseEntity<ResponseDTO> handleEmployeePayrollException(
+										EmployeePayrollException exception) {
+		ResponseDTO responseDTO = new ResponseDTO("Exception while processing REST Request",
+													exception.getMessage());
+		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
+	}
 	
 }
