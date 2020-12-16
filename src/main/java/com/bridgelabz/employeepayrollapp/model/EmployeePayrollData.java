e02c5128a70f8e3127ac388a8dfb65cf76e76e40
madhu179 <madhubabu232343@gmail.com>
2020-12-16 15:29:41 +0530

    Adding code to add ability to save employee payroll data to Mysql DB

--- src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
@@ -3,24 +3,47 @@ package com.bridgelabz.employeepayrollapp.model;
 import java.time.LocalDate;
 import java.util.List;
 
+import javax.persistence.JoinColumn;
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
 
 import lombok.Data;
 
+@Entity
+@Table(name="employee_payroll")
 public @Data class EmployeePayrollData {
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="employee_id")
 	private int employeeId;
+	
 	private String name;
 	private long salary;
 	private String gender;
+	
+	@Column(name="start_date")
 	private LocalDate startDate;
+	
 	private String note;
+	
+	@Column(name="profile_pic")
 	private String profilePic;
+	
+	@ElementCollection
+	@CollectionTable(name="employee_department", joinColumns = @JoinColumn(name="id"))
 	private List<String> departments;
 	
 	
-	public EmployeePayrollData(int employeeId, EmployeePayrollDTO empPayrollDTO) {
-		this.employeeId = employeeId;
+	public EmployeePayrollData( EmployeePayrollDTO empPayrollDTO) {
 		this.updateEmployeePayrollDTO(empPayrollDTO);
 	}
 
--- src/main/java/com/bridgelabz/employeepayrollapp/service/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/service/EmployeePayrollService.java
@@ -3,14 +3,22 @@ package com.bridgelabz.employeepayrollapp.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
 import com.bridgelabz.employeepayrollapp.exceptions.EmployeePayrollException;
 import com.bridgelabz.employeepayrollapp.model.EmployeePayrollData;
+import com.bridgelabz.employeepayrollapp.repository.EmployeePayrollRepository;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Service
+@Slf4j
 public class EmployeePayrollService implements IEmployeePayrollService {
+	
+	@Autowired
+	private EmployeePayrollRepository employeeRepository;
 	
 private List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
 	
@@ -31,9 +39,10 @@ private List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
 	@Override
 	public EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO empPayrollDTO) {
 		EmployeePayrollData empData = null;
-		empData = new EmployeePayrollData(employeePayrollList.size()+1, empPayrollDTO);	
+		empData = new EmployeePayrollData(empPayrollDTO);	
 		employeePayrollList.add(empData);
-		return empData;
+		log.debug("Emp Data: "+empData.toString());
+		return employeeRepository.save(empData);
 	}
 
 	@Override
